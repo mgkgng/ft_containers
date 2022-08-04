b4317ce5862b53dcda71e54bfda8e64c52dfd0ca
Minguk Gaang <minguk.gaang@gmail.com>
2022-08-04 17:28:54 +0200

    rbnode ready

--- srcs/RBTree.hpp
+++ srcs/RBTree.hpp
@@ -36,7 +36,7 @@ class RBnode {
 			return (gp->_left);
 		}
 
-		RBnode *getSibling()
+		RBnode *getS()
 		{
 			if (this == _parent->_left)
 				return (_parent->_right);
@@ -132,7 +132,114 @@ class RBnode {
 				(p->right == this) ? p->right = c : p->left = c;
 		}
 
-}
+		//////////////////
+		// ** delete ** //
+		//////////////////
+
+		void delete_one_child()
+		{
+			RBnode *child = (_right == LEAF) ? _left: _right;
+
+			replace_node(child);
+			if (_color == BLACK)
+				(child->color == RED) ? child->color = BLACK : delete_case1(child);
+			free(this);
+		}
+
+		void delete1()
+		{
+			if (_parent)
+				delete2();
+		}
+
+		void delete2()
+		{
+			RBnode *s = getS();
+
+			if (s->_color == RED) {
+				_parent->_color = RED;
+				s->_color = BLACK;
+				if (this == _parent->left)
+					_parent->rotateL();
+				else
+					_parent->rotateR();
+			}
+			delete3();
+		}
+
+		void delete3()
+		{
+			RBnode *s = getS();
+
+			if (_parent->_color == BLACK && s->_color == BLACK
+				&& s->_left->_color == BLACK && s->_right->_color == BLACK) {
+				s->_color = RED;
+				_parent->delete1();
+			} else
+				delete4();
+		}
+
+		void delete4(struct node *n)
+		{
+			RBnode *s = getS(n);
+
+			if (_parent->_color == RED && s->_color == BLACK
+				&& s->_left->_color == BLACK && s->_right->_color == BLACK) {
+				s->_color = RED;
+				_parent->_color = BLACK;
+			} else
+				delete5();
+		}
+
+		void delete5()
+		{
+			RBnode *s = getS();
+
+			if  (s->_color == BLACK) {
+				if (this == _parent->_left && s->_right->_color == BLACK
+					&& s->_left->_color == RED) { /* this last test is trivial too due to cases 2-4. */
+					s->_color = RED;
+					s->_left->_color = BLACK;
+					s->rotateR();
+				} else if (this == _parent->_right && s->_left->_color == BLACK
+					&& s->_right->_color == RED) {/* this last test is trivial too due to cases 2-4. */
+					s->_color = RED;
+					s->_right->+color = BLACK;
+					s->rotateL();
+				}
+			}
+			delete6();
+		}
+
+		void delete6()
+		{
+			RBnode *s = getS();
+
+			s->_color = _parent->_color;
+			_parent->_color = BLACK;
+
+			if (this == _parent->_left) {
+				s->_right->_color = BLACK;
+				rotateL(_parent);
+			} else {
+				s->_left->_color = BLACK;
+				rotateR(_parent);
+			}
+		}
+
+		//////////////////
+		// ** utils ** //
+		//////////////////
+
+		void replace(RBnode *n, RBnode *child)
+		{
+			child->_parent = n->_parent;
+			if (n->_parent->_left == n)
+				n->_parent->_left = child;
+			else if (n->_parent->_right == n)
+				n->_parent->_right = child;
+		}
+};
 
 class RBtree {
 
